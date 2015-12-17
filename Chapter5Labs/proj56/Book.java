42ee5bb164e376d28e00bbebe906c8e321ff0fa8
taylanu <taylanu@users.noreply.github.com>
2015-12-16 19:53:33 -0500

    "commit one, 12/16/15"

---
+++ Chapter5Labs/proj56/Book.java
@@ -0,0 +1,32 @@
+// Regional National International Library of Tay
+public class Book
+{
+   private String title, author;
+   //The Book
+   public Book()
+   {
+      title = "no title";
+      author = "no author";
+   }
+   public Book(String t, String a)
+   {
+      title = t;
+      author = a;
+   }
+   
+   // The Retrievers
+   public String getTitle()
+   {  
+      return title;
+   }
+   
+   public String getAuthor()
+   {  
+      return author;
+   }
+   //Returner
+   public String toString()
+   {
+      return (title + " by " + author);
+   } 
+}
