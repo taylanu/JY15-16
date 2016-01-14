f9ad9a4058ec2cdc5a0215dba44e0270393eed67
Taylan <taylanu@users.noreply.github.com>
2016-01-14 16:10:29 -0500

    Update Checking.java

--- Chapter10Labs/proj105/Checking.java
+++ Chapter10Labs/proj105/Checking.java
@@ -2,9 +2,33 @@ package proj105;
 //Taylan Unal 1/14/16
 
 public class Checking extends BankAccount{
-
-	public Checking(){
-		// TODO Auto-generated constructor stub
-	}
+	private double balance;
+	private double interest;
+	private int txlimit;
+	
+		public Checking(){
+		balance="0.00";
+		interest="0.00";
+		txlimit="999";
+		}
+		
+		public double getBalance(){
+			return "$" + balance;
+		}
+		public double getInterest(){
+			return (interest*100) + "%";
+		}
+		public double getTxLimit(){
+			return txlimit;
+		}
+		public void setBalance(double b){
+			balance=b;
+		}
+		public void setInterest(double i){
+			interest=i;
+		}
+		public void setTxLimit(int tx){
+			txlimit=tx;
+		}
 
 }
