8879e3f9a342f52f1abaadde95e8987e84310871
Taylan <taylanu@gmail.com>
2016-02-18 12:09:10 -0500

    2/18/16

--- Chapter11Labs/proj112/intArraySort.java
+++ Chapter11Labs/proj112/intArraySort.java
@@ -1,6 +1,6 @@
 package proj112;
 
-public class proj112 {
+public class intArraySort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- Chapter11Labs/proj113/proj113.java
+++ Chapter11Labs/proj113/proj113.java
@@ -1,10 +1,13 @@
 package proj113;
 
+import java.io.*;
+import java.util.*;
+
 public class proj113 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner fileReader = new Scanner(new File("/*File name*/"));
 	}
 
 }
