eb2bf533c321066d7978092fbb9cfe4b35b79dd7
taylanu <taylanu@localhost>
2016-03-18 11:01:25 -0400

    TidBits

--- Chapter12Labs/Towers/visualTowers.java
+++ Chapter12Labs/Towers/visualTowers.java
@@ -7,7 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class towers extends JFrame {
+public class visualTowers extends JFrame {
 
 	private JPanel contentPane;
 
@@ -18,7 +18,7 @@ public class towers extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					towers frame = new towers();
+					visualTowers frame = new visualTowers();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +30,7 @@ public class towers extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public towers() {
+	public visualTowers() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
--- Chapter12Labs/superPrime/superPrime.java
+++ Chapter12Labs/superPrime/superPrime.java
@@ -6,18 +6,28 @@ public class superPrime {
 	
 static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("Enter a number");
+		System.out.println("Enter a positive number");
 		int usernum = input.nextInt(); 
-		System.out.println("Your number " + sPrime(usernum) + " is a SuperPrime");
-		
-	}
-	public static long sPrime(long num){
-		if(num%num==0)
-			return num;
+		if(sPrime(usernum)==false)
+			System.out.print(usernum + " is not a SuperPrime");
 		else
-			return sPrime(num/10);
-		//how to separate the parts of number??
-		//solved.
+			System.out.print(usernum + " is a SuperPrime");
+	}
+	public static boolean sPrime(long num){
+		if((num<10) && (isPrime(num)))
+			return true;
+		else if(isPrime(num))
+			return sPrime(num/10);//div by 10, cuts off last digit
+		else
+			return false;
 		}
-//div by 10, cuts off last digit
+	public static boolean isPrime(long n){
+	    if(n==1)
+	    	return false;//1 isnt technically a prime number
+		for(int i=2;i<n/2;i++) {
+	        if(n%i==0)
+	             return false;
+	    }
+	    return true;
+	}
 }
